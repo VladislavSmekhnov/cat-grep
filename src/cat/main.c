feaf32f9a17d1320822b87f3c8996ff52067d688
Phaedra Cerrick <phaedrac@et-a4.kzn.21-school.ru>
2022-11-05 18:15:39 +0300

    Parser problems

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct opt{
+typedef struct options {
   int b;
   int e;
   int v;
@@ -19,24 +19,102 @@ void zero_flags(opt *flags) {
   flags->t = 0;
 }
 
-int argv_parser(char** argv, opt* flags);
+int argv_parser(int argc, char** argv, opt* flags);
 
 int main(int argc, char** argv) {
+  int state = 0;
   if (argc > 1) {
+    printf("argv[0] = %s\nargv[1] = %s\nargv[2] = %s\n", argv[0], argv[1], argv[2]);
     opt flags;
     zero_flags(&flags);
-    argv_parser(argv, &flags);
+    if (argv_parser(argc, argv, &flags) == 0) {
+      int count = 1;
+      printf("%s\n", argv[count]);
+      while (argv[count][1] != '\0') {
+        FILE *fptr;
+        if ((fptr = fopen(argv[count], "r")) != NULL) {
+          char print_char = getc(fptr);
+          char futur_char = '\0';
+          int line_number = 1;
+          // if we have n flag firstly run:
+          if (print_char != EOF) {
+            if (flags.n) {
+              printf("%6d\t", line_number);
+              line_number++;
+            }
+          }
+          while ((futur_char = getc(fptr)) != EOF) {
+            // if s:
+            if (print_char == '\n' && futur_char == '\n') {
+              while (futur_char == '\n') {
+                futur_char = getc(fptr);
+              }
+            }
+            if (print_char != EOF) {
+              // print:
+              if (flags.t) {
+                if (flags.v) {
+                  if (print_char >= 0 && print_char < 32)
+                    printf("^%c", print_char + 64);
+                  else if (print_char == '\t')
+                    printf("^I");
+                  else
+                    printf("%c", print_char);
+                } else if (print_char == '\t') {
+                  printf("^I");
+                } else {
+                  printf("%c", print_char);
+                }
+              }
+              if (flags.v) {
+                if (print_char >= 0 && print_char < 32)
+                  printf("^%c", print_char + 64);
+                else
+                  printf("%c", print_char);
+              }
+              // if n:
+              if (flags.n) {
+                printf("%6d\t", line_number);
+                line_number++;
+              }
+              // if e:
+              if (flags.e) {
+                if (flags.v) {
+                  if (print_char >= 0 && print_char < 32)
+                    printf("^%c", print_char + 64);
+                  else if (print_char == '\n')
+                    printf("$");
+                  else
+                    printf("%c", print_char);
+                } else if (print_char == '\n') {
+                  printf("$");
+                }
+              }
+            }
+            print_char = futur_char;
+          }
+          if (print_char != EOF)
+            printf("%c", print_char);
+
+        } else {
+          state = 1;
+        }
+      }
+    }
   }
-  return 0;
+  return state;
 }
 
-int argv_parser(char** argv, opt* flags) {
+int argv_parser(int argc, char** argv, opt* flags) {
   int count = 1;
   int state = 0;
 
+  printf("Inside parser:\n");
   while (argv) {
     if (argv[count][0] == '-' && argv[count][1] != '-') {
-      for (size_t i = 0; i < strlen(argv[count]); ++i) {
+      printf("strlen(argv[count]) = %ld\n", strlen(argv[count]));
+      for (size_t i = 1; i < strlen(argv[count]); ++i) {
+        printf("%c\n", argv[count][i]);
         if (argv[count][i] == 'b') {
           flags->b = 1;
         } else if (argv[count][i] == 'e') {
@@ -53,7 +131,7 @@ int argv_parser(char** argv, opt* flags) {
           flags->v = 1;
         } else if (argv[count][i] == 'T') {
           flags->t = 1;
-        } else {
+        } else if (i == 1) {
           state = 1;
         }
       }
@@ -68,8 +146,10 @@ int argv_parser(char** argv, opt* flags) {
         state = 2;
       }
     }
-    memset(argv[count], '\0', strlen(argv[count]));
     count++;
+  }
+  for (int i = 1; i < argc; ++i) {
+    memset(argv[i], '\0', strlen(argv[i]));
   }
 
   return state;
